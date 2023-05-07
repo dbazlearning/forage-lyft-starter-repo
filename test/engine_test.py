38ca43fb97a0162fa364a67c09424c0b5bf64c3f
DeveloppeurMG <developpe@mgdigitalplus.com>
2023-05-07 12:47:46 +0200

    Firest

--- test/engine_test.py
+++ test/engine_test.py
@@ -1,1 +1,10 @@
 
+from engine.engine_type.capulet_engine import CapuletEngine
+from engine.engine_type.sternman_engine import SternmanEngine
+from engine.engine_type.willoughby_engine import WilloughbyEngine
+
+import unittest
+
+class TestWilloughbyEngine(unittest.TestCase):
+    def test_needs_service_true(self):
+        pass
